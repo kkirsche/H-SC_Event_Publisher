971772dcede281142261f086f4bbfa2862c14d40
Kevin Kirsche <kkirsche@hsc.edu>
2013-06-13 13:18:10 -0400

    Updated front end template

--- templates/vanilla/Frontend.tpl.php
+++ templates/vanilla/Frontend.tpl.php
@@ -1,18 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
     <head>
         <title><?php echo $this->calendar->name; ?></title>
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->uri; ?>templates/vanilla/screen.css" />
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->uri; ?>templates/vanilla/css/fullcalendar.css" />
-        <?php 
+        <?php
         if (!isset($this->calendar->theme)) {
             $this->calendar->theme = "base";
         }
         ?>
         <link rel="stylesheet" type="text/css" media="screen" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/themes/<?php echo $this->calendar->theme ?>/jquery-ui.css" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
         <script src="<?php echo $this->uri; ?>templates/vanilla/javascript/fullcalendar.min.js"></script>
     </head>
     <body>
@@ -23,7 +22,7 @@
 
         <?php UNL_UCBCN::displayRegion($this->output); ?>
 
-<script type='text/javascript'> 
+<script type='text/javascript'>
 $(document).ready(function() {
     $('.calendar').fullCalendar({
         theme: true,
@@ -43,13 +42,6 @@ $(document).ready(function() {
 </script>
 
         <div class="footer">
-            <h3>Yeah, It's Open Source</h3>
-                The University Event Publishing System is an open source project
-                built by the <a href="http://www.unl.edu/">University of Nebraska&ndash;Lincoln</a>
-                which implements the UC Berkeley Calendar specifications.
-            <ul>
-                <li><a href="http://code.google.com/p/unl-event-publisher/">UNL Event Publisher</a></li>
-            </ul>
         </div>
     </body>
 </html>
